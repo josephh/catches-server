02689885bb4b191aa8cf4d23c57d07612c953b57
josephh <josephh@rocketmail.com>
2017-03-05 20:24:48 +0000

    notes and implementation

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ var tags = require('./plugins/tags'); /* for really terse code, this could be
                                        * required by seneca in its use() method.
                                        */
 
-require('seneca')()
-  /* seneca.use method takes a plugin name parameter and an optional options
-   * parameter to be passed into the plugin
-   */
-  .use(tags, {logfile: './tags.log'})
-  /* seneca.act sends a message to act on. It has 2 parameters
-   * 1. the message object
-   * 2. response_callback: function that receives the response message
-   * (if any comes back from the act action)
-   */
-  .act('role:crud,cmd:fetchTags,filterBy:angler', console.log) // 'role:retrieve' is just a namespacing convention
+// require('seneca')()
+//   /* seneca.use method takes a plugin name parameter and an optional options
+//    * parameter to be passed into the plugin
+//    */
+//   .use(tags, {logfile: './tags.log'})
+//   /* seneca.act sends a message to act on. It has 2 parameters
+//    * 1. the message object
+//    * 2. response_callback: function that receives the response message
+//    * (if any comes back from the act action)
+//    */
+//   .act('role:crud,cmd:fetchTags,filterBy:angler', console.log) // 'role:retrieve' is just a namespacing convention
+
+  require('seneca')()
+    .use(tags, {logfile: './tags.log'})
+    .listen(9000);
