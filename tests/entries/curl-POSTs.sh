a0725a8f56893c7d6763f909a982aa86e8e474e5
josephh <josephh@rocketmail.com>
2017-09-01 22:10:13 +0100

    tweak test data and upload scripts

--- tests/entries/curl-POSTs.sh
+++ tests/entries/curl-POSTs.sh
@@ -1,11 +1,51 @@
+#! /bin/bash
 
-curl -v -H "Content-Type: multipart/form-data" -F data='{"newCatch":{"species":"pike","date":"2017-01-05T08:46:27Z","userId":"1","coordinates":{"lat":"51.489921","long":"-0.23182869"},"tags":[{"type":"angler","value":"jon"},{"type":"location","value":"hammersmith"},{"type":"location","value":"thames"}]}}' -F "image=@./1/1.jpg" http://localhost:8000/api/catches
-curl -v -H "Content-Type: multipart/form-data" -F "data=@./2/2.json" -F "image=@./2/2.jpg" http://localhost:8000/api/catches
-curl -v -H "Content-Type: multipart/form-data" -F "data=@./3/3.json" -F "image=@./3/3.jpg" http://localhost:8000/api/catches
-curl -v -H "Content-Type: multipart/form-data" -F "data=@./4/4.json" -F "image=@./4/4.jpg" http://localhost:8000/api/catches
-curl -v -H "Content-Type: multipart/form-data" -F "data=@./5/5.json" -F "image=@./5/5.jpg" http://localhost:8000/api/catches
-curl -v -H "Content-Type: multipart/form-data" -F "data=@./6/6.json" -F "image=@./6/6.jpg" http://localhost:8000/api/catches
-curl -v -H "Content-Type: multipart/form-data" -F "data=@./7/7.json" -F "image=@./7/7.jpg" http://localhost:8000/api/catches
-curl -v -H "Content-Type: multipart/form-data" -F "data=@./8/8.json" -F "image=@./8/8.jpg" http://localhost:8000/api/catches
-curl -v -H "Content-Type: multipart/form-data" -F "data=@./9/9.json" -F "image=@./9/9.jpg" http://localhost:8000/api/catches
-curl -v -H "Content-Type: multipart/form-data" -F "data=@./10/10.json" -F "image=@./10/10.jpg" http://localhost:8000/api/catches
+curl -X POST http://localhost:8000/api/catches \
+  -H "Content-Type: multipart/form-data" \
+  -F "data=$(cat ./1/1.json)" \
+  -F "image=@./1/1.jpg"
+
+curl -i -X POST http://localhost:8000/api/catches \
+  -H "Content-Type: multipart/form-data" \
+  -F "data=$(cat ./2/2.json)" \
+  -F "image=@./2/2.jpg"
+
+curl -i -X POST http://localhost:8000/api/catches \
+  -H "Content-Type: multipart/form-data" \
+  -F "data=$(cat ./3/3.json)" \
+  -F "image=@./3/3.jpg"
+
+curl -i -X POST http://localhost:8000/api/catches \
+  -H "Content-Type: multipart/form-data" \
+  -F "data=$(cat ./4/4.json)" \
+  -F "image=@./4/4.jpg"
+
+curl -i -X POST http://localhost:8000/api/catches \
+  -H "Content-Type: multipart/form-data" \
+  -F "data=$(cat ./5/5.json)" \
+  -F "image=@./5/5.jpg"
+
+curl -i -X POST http://localhost:8000/api/catches \
+  -H "Content-Type: multipart/form-data" \
+  -F "data=$(cat ./6/6.json)" \
+  -F "image=@./6/6.jpg"
+
+curl -i -X POST http://localhost:8000/api/catches \
+  -H "Content-Type: multipart/form-data" \
+  -F "data=$(cat ./7/7.json)" \
+  -F "image=@./7/7.jpg"
+
+curl -i -X POST http://localhost:8000/api/catches \
+  -H "Content-Type: multipart/form-data" \
+  -F "data=$(cat ./8/8.json)" \
+  -F "image=@./8/8.jpg"
+
+curl -i -X POST http://localhost:8000/api/catches \
+  -H "Content-Type: multipart/form-data" \
+  -F "data=$(cat ./9/9.json)" \
+  -F "image=@./9/9.jpg"
+
+curl -i -X POST http://localhost:8000/api/catches \
+  -H "Content-Type: multipart/form-data" \
+  -F "data=$(cat ./10/10.json)" \
+  -F "image=@./10/10.jpg"
